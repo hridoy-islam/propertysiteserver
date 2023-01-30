c66378398b472ad4815bbe3276961b3e08c05d46
Hridoy Islam <hridoy4t@gmail.com>
2023-01-30 07:55:32 +0600

    last from ubuntu

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 5000
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@peropertycluster.ocyefgu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.use(cors())
 express.json();
@@ -9,6 +13,22 @@ express.json();
 app.get('/', (req, res) => {
   res.send('Property Backend Running')
 })
+
+async function run() {
+  try {
+      app.post('/jsonToken', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ token })
+    })
+
+  }
+  finally {
+
+  }
+}
+
+run().catch(error => console.log(error))
 
 app.listen(port, () => {
   console.log(`Server Backend Running on ${port}`)
